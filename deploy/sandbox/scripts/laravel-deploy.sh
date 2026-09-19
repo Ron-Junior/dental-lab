4105177fd459987779d2e6908ec7be404fe1756d
root <root@RonaldoCosta.localdomain>
2026-09-19 13:47:03 -0300

    add new deploy flow

---
+++ deploy/sandbox/scripts/laravel-deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Caching views..."
+php artisan view:cache
+
+echo "Running migrations..."
+php artisan migrate --force
