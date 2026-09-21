035662e302850893a80f8cc2371c6e5a36933f6b
root <root@RonaldoCosta.localdomain>
2026-09-21 20:18:12 -0300

    change the environment validation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') === 'production' || config('app.env') === 'sandbox') {
+        if ($this->app->environment('production', 'sandbox')) {
             URL::forceScheme('https');
         }
     }
